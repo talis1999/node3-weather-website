223f3689151f7ce876d2f1093cefbce758b46638
Daniel Talisman <dantalisman@gmail.com>
2020-04-18 06:07:46 +0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for Express config
 const publicPath = path.join(__dirname, '../public')
@@ -89,10 +90,7 @@ app.get('*', (req,res) => {
         name:'Daniel Talisman'
     })
 })
-//app.com
-//app.com/help
-//app.com/about
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port '+ port)
 })
